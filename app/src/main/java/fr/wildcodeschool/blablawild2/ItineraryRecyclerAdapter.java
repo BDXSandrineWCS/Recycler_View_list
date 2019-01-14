f0b58ebe02bddc03c1432a7a79d1149cd359b919
sandrine <poupardsandrine@gmail.com>
2019-01-14 16:47:02 +0100

    Créer une liste avec RecyclerView quest finished

--- app/src/main/java/fr/wildcodeschool/blablawild2/ItineraryRecyclerAdapter.java
+++ app/src/main/java/fr/wildcodeschool/blablawild2/ItineraryRecyclerAdapter.java
@@ -2,32 +2,58 @@ package fr.wildcodeschool.blablawild2;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class ItineraryRecyclerAdapter extends RecyclerView.Adapter {
+public class ItineraryRecyclerAdapter extends RecyclerView.Adapter<ItineraryRecyclerAdapter.ViewHolder> {
 
     private ArrayList<ItineraryModel> itineraries;
 
     public ItineraryRecyclerAdapter(ArrayList<ItineraryModel> itineraries) {
-        itineraries = itineraries;
+        this.itineraries = itineraries;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // TODO : associer le layout utilisé par un item
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_itinerary, parent, false);
+
+        return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        // TODO : action à l'affichage d'un item
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ItineraryModel itineraryModel = itineraries.get(position);
+        holder.driver.setText(itineraryModel.getDriver());
+        holder.date.setText(itineraryModel.getDate().toString());
+        holder.price.setText(String.valueOf(itineraryModel.getPrice()));
     }
+
 
     @Override
     public int getItemCount() {
         return itineraries.size();
     }
+
+    protected ItineraryModel getItemAt(int position){
+        return itineraries.get(position);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView date, driver, price;
+
+        public ViewHolder(View v) {
+            super(v);
+            this.date = v.findViewById(R.id.tv_date);
+            this.driver = v.findViewById(R.id.tv_driver);
+            this.price = v.findViewById(R.id.tv_price);
+        }
+    }
+
 }
